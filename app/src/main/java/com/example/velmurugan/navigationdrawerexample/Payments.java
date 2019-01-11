72308f8273dc1aad8130abd793633d12b789b1d3
johnson <johnsonkhristi6029@gmail.com>
2019-01-11 15:43:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/velmurugan/navigationdrawerexample/Payments.java
@@ -0,0 +1,16 @@
+package com.example.velmurugan.navigationdrawerexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Payments extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.payments,container,false);
+    }
+}
